5fb132a6060dcb24655d0eeedf0701c547225f5e
ridip <ridipranjandeka35@gmail.com>
2022-10-12 01:31:15 -0700

    new.c

---
+++ new.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+  int number;
+
+  printf("enter an integer:");
+  scanf("%d",&number);
+
+  if(number<0)
+  {
+
+    printf("You entered %d.\n",number);
+  }
+
+  printf("the if statement is easy.");
+  return 0;
+  
+}
